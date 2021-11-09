beb2ff077c7f1adea140850a7bf5327ac415b0db
gregorylira <gregory.lira007@gmail.com>
2021-11-09 14:41:26 -0300

    basico front

---
+++ frontend/src/components/templates/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import "./Main.css"
+
+
+export default props=>
+    <React.Fragment>
+        <Header />
+        <main className="content">
+            Conteudo
+        </main>
+    </React.Fragment>
---
+++ frontend/src/components/templates/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Nav.css"
+
+export default props => 
+    <nav className="menu-area">
+        Navegação
+    </nav>
