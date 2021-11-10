cfb15bc94167d74cc69f20b723b3bbb91668ddb1
gregorylira <gregory.lira007@gmail.com>
2021-11-09 21:29:35 -0300

    sistema de rotas user e home

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,2 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import Main from "../templates/Main";
+
+const headerProps = {
+    icon: "users",
+    title: "Usuários",
+    subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir"
+}
+
+export default class UserCrud extends Component {
+    render(){
+        return (
+            <Main {...headerProps}>
+                Cadastro de Usuário
+            </Main>
+        )
+    }
+}
